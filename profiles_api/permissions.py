7d1e2202bc0b4679a11390734f94f5f9e99fbfd9
edualt <eduardosebastianhdz503@gmail.com>
2022-05-16 12:40:33 -0500

    feat(permissions): UpdateOwnStatus added

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -12,4 +12,13 @@ class UpdateOwnProfile(permissions.BasePermission):
         
         return obj.id == request.user.id
 
-        
+class UpdateOwnStatus(permissions.BasePermission):
+    """ Permite actualizar propio status feed """
+
+    def has_object_permission(self, request, view, obj):
+        """ Chequear si usuario esta intentando editar su propio perfil """
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile_id == request.user.id
